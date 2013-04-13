985549342304cf753f48c5fdfc56ea4bf843c5a3
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2013-04-13 04:08:34 -0700

    Some fix

--- client/conversation.js
+++ client/conversation.js
@@ -10,11 +10,11 @@ Conversation = new Meteor.Collection("Conversations");
 
 Template.conversations.all = function() {
   var codeSessionId = Session.get("codeSessionId");
-  var allConversation = Conversation.find({codeSessionId: codeSessionId});
+  var allConversation = Conversation.find().fetch();
   var result = "";
   for (var i = 0; i < allConversation.length; i++) {
-    console.log(allConversation[i].message);
     result += allConversation[i].message + "\n";
+    console.log(result);
   }
   return result;
 }
@@ -24,7 +24,7 @@ Template.conversations.events = {
   'click #sendBtn' : function () {
     var text = $("#message").val();
     var codeSessionId = Session.get("codeSessionId");
-    Conversation.insert({message: text, codeSessionId: codeSessionId});
+    var id = Conversation.insert({message: text, codeSessionId: codeSessionId});
   }
 }
 
--- server/Conversation.js
+++ server/Conversation.js
@@ -6,4 +6,3 @@
  * To change this template use File | Settings | File Templates.
  */
 
-Conversation = new Meteor.Collection("Conversations");
