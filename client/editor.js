111f842c3ba8cc19bc2639c471ac3a5c18486f17
clydeli <clyde1008li@gmail.com>
2013-04-13 00:30:40 -0700

    add editor component

--- client/editor.js
+++ client/editor.js
@@ -1,5 +1,10 @@
+// Meteor Logics
 Template.editor.codeSession = function() {
   var codeSessionId = Session.get("codeSessionId");
   return CodeSession.findOne({_id: codeSessionId});
 };
 
+// Manual Manipulation
+var editorInstance = ace.edit("editor");
+editorInstance.setTheme("ace/theme/monokai");
+editorInstance.getSession().setMode("ace/mode/javascript");
