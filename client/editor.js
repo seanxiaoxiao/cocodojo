885ef2b8c7e1f7e1f71398cc6c9d59cc7b3f1d7b
clydeli <clyde1008li@gmail.com>
2013-04-13 05:17:51 -0700

    initial try of editor syncing

--- client/editor.js
+++ client/editor.js
@@ -4,10 +4,39 @@ Template.editor.codeSession = function() {
   return CodeSession.findOne({_id: codeSessionId});
 };
 
-// Manual Manipulation
 Template.editor.rendered = function() {
-  editorInstance = ace.edit("editorInstance");
-  editorInstance.setTheme("ace/theme/monokai");
-  editorInstance.getSession().setMode("ace/mode/javascript");
+  cocodojo.editor = {};
+  cocodojo.editor.updateDue = false;
+  cocodojo.editor.local_uid = (((1+Math.random())*0x10000)|0).toString(16).slice(1);
+  cocodojo.editor.editorInstance = ace.edit("editorInstance");
+  cocodojo.editor.editorInstance.setTheme("ace/theme/monokai");
+  cocodojo.editor.editorInstance.getSession().setMode("ace/mode/javascript");
+
+
+  // Manual Manipulation
+  cocodojo.editor.editorInstance.getSession().getDocument().on("change", function(e){
+    if(cocodojo.editor.updateDue){ cocodojo.editor.updateDue = false; }
+    else{
+      console.log(e);
+      CodeSession.update(
+        {_id: Session.get("codeSessionId")}, 
+        { $set: 
+          { newDelta: e.data,//cocodojo.editor.editorInstance.getValue(),
+            sender_uid: cocodojo.editor.local_uid }
+        }
+      );
+    }
+  });
+
+  var mongoQuery = CodeSession.find({_id: Session.get("codeSessionId")});
+  mongoQuery.observe({
+    changed : function(newDoc, oldIndex, oldDoc) {
+      if(newDoc.sender_uid !== cocodojo.editor.local_uid){
+        //console.log(newDoc.newDelta);
+        cocodojo.editor.updateDue = true;
+        cocodojo.editor.editorInstance.getSession().getDocument().applyDeltas([newDoc.newDelta]);
+      }
+    }
+  });
 
 };
--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 
 if (Meteor.isClient) {
   CodeSession = new Meteor.Collection("CodeSessions");
-
+  cocodojo = {};
   Meteor.startup(function () {
     Backbone.history.start({pushState: true});
     $(document).ready(function() {
